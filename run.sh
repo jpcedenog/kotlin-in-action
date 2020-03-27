010652fa4b1edc98981858f001c6f59a0aaf4a31
JP Cedeno <jpcedenog@gmail.com>
2020-03-26 22:17:22 -0400

    Updates

--- run.sh
+++ run.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-clear
-gradle build
-#kotlin -classpath build/libs/basic.jar chapters.chapter5.S55Kt
+clear &&
+gradle build &&
 kotlin -classpath build/libs/basic.jar chapters.chapter$1.S$1$2Kt
